b4267875465f1312580f99caeb9a50dee4dafd7e
georgethedeveloper77 <georgekarani77@gmail.com>
2020-11-27 16:03:55 +0300

    uberclone

---
+++ ridealong2_client/android/app/src/main/kotlin/com/mindhunter/ridealong2_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mindhunter.ridealong2_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ridealong2_driver/android/app/src/main/kotlin/com/mindhunter/ridealong2_driver/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mindhunter.ridealong2_driver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
